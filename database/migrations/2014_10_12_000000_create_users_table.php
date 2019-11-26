5ab506bc59870ab138f5e82bf0a4c81ba0b86d7c
Sheikh Md Sabbir Hossain <sabbirjoy8@gmail.com>
2019-11-27 01:21:21 +0600

    table users,pass reset

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('isadmin')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
