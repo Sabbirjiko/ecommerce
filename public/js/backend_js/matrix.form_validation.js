18e149976e33ee5c371bff7a52c6f971256fecd8
Sheikh Md Sabbir Hossain <sabbirjoy8@gmail.com>
2019-11-27 00:33:46 +0600

    backend Js
    Js Used in Backend

---
+++ public/js/backend_js/matrix.form_validation.js
@@ -0,0 +1,171 @@
+
+$(document).ready(function(){
+	$('input[type=checkbox],input[type=radio],input[type=file]').uniform();
+	
+	$('select').select2();
+	$('#current_pwd').keyup(function() {
+		var current_pwd = $('#current_pwd').val();
+		$.ajax({
+			type:'get',
+			url:'/admin/check_pass',
+			data:{current_pwd:current_pwd},
+			success: function(resp) {
+				if (resp == "False") {
+					$("#pwd_info").html("<font color='red'>Password Incorrect</font>");
+				}else{
+					$("#pwd_info").html("<font color='green'>Password Matched</font>");
+				}
+
+            },
+            error: function(resp) {
+ 				alert('False');
+            }
+		});
+	});
+	//Category form Validation 
+    $("#add_category").validate({
+		rules:{
+			required:{
+				required:true
+			},
+			category_name:{
+				required:true,
+			},
+			description:{
+				required:true,
+			},
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+
+	//Category form Validation 
+    $("#add_product").validate({
+		rules:{
+			required:{
+				required:true
+			},
+			product_name:{
+				required:true,
+			},
+			product_code:{
+				required:true,
+			},
+			product_color:{
+				required:true,
+			},
+			price:{
+				required:true,
+				number:true,
+			},
+			image:{
+				required:true,
+			},
+			category_id:{
+				required:true,
+			},
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+
+	// Form Validation
+    $("#basic_validate").validate({
+		rules:{
+			required:{
+				required:true
+			},
+			email:{
+				required:true,
+				email: true
+			},
+			date:{
+				required:true,
+				date: true
+			},
+			url:{
+				required:true,
+				url: true
+			}
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+	
+	$("#number_validate").validate({
+		rules:{
+			min:{
+				required: true,
+				min:10
+			},
+			max:{
+				required:true,
+				max:24
+			},
+			number:{
+				required:true,
+				number:true
+			}
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+	
+	$("#password_validate").validate({
+		rules:{
+			current_pwd:{
+				required: true,
+				minlength:5,
+				maxlength:20
+			},
+			new_pwd:{
+				required: true,
+				minlength:6,
+				maxlength:20
+			},
+			confirm_pwd:{
+				required:true,
+				minlength:6,
+				maxlength:20,
+				equalTo:"#new_pwd"
+			}
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+});
