82d82afec3c8a1f7d49384b71c85174becf0201a
Sheikh Md Sabbir Hossain <sabbirjoy8@gmail.com>
2019-11-27 00:47:54 +0600

    Database of product and category
    Database of product and category

---
+++ database/migrations/2019_11_21_201101_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->string('product_name');
+            $table->string('slug');
+            $table->string('product_code');
+            $table->string('product_color');
+            $table->text('description');
+            $table->float('price');
+            $table->string('image');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
